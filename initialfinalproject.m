a03c587cda80e2940632fe7494eab1d6abe86c15
ceeville <30543811+ceeville@users.noreply.github.com>
2022-11-22 15:53:49 -0800

    Add files via upload

---
+++ initialfinalproject.m
@@ -0,0 +1,24 @@
+data = readmatrix("initialRAWdata.dat");
+dininghall = data(:,1);
+day = data(:,2);
+meal = data(:,3);
+nomeat = data(:,6);
+busy = data (:,7);
+dimension = size(data);
+% need to create a while loop which will check the day and create a vector
+% based off of the day
+sunday = [1;
+    1];
+% initializing sunday matrix
+counter = 1;
+for i = 1:dimension(1,1)
+    if data(i,2) == 1
+        % when the day is sunday
+        sunday(counter,1) = data(i,4)
+        counter = counter +1;
+    end
+end
+% ratingavg = sum(rating,dimension(1,1));
+% repeat this function but with a different vector to find the average
+
+
